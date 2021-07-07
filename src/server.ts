24d7ac8105299574bfc6e5205f535c5fcbd7903d
Anna Clara <anisferraz@gmail.com>
2021-07-07 13:36:13 -0300

    initializing database

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,7 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
 
 const app = express();
-
-/*
-GET     => Buscar uma informação
-POST    => Inserir (criar) uma informação
-PUT     => Alterar uma informação
-DELETE  => Remover um dado
-PATCH   => Alterar uma informação específica 
-*/
-
-app.get("/test", (req, res) => { return res.send("Olá") })
-app.post("/testpost", (req, res) => { return res.send("Olá, testando metodo post")})
 
 app.listen(3000, () => console.log("server is running"));
